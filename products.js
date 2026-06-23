9d73971b6124a4b93378b9c87621aa61ae1c3147
groundedcogoa <rutujaasheth@gmail.com>
2026-06-23 19:08:44 +0530

    products.js

---
+++ products.js
@@ -0,0 +1,80 @@
+// CONFIGURATION FOR NON-TECH TEAM: Edit names, SKUs, or Sale Rates safely here.
+// IMPORTANT DIRECTIONS FOR THE TEAM:
+// 1. Do NOT delete the square brackets [ ] or curly braces { }.
+// 2. Make sure every item has a comma (,) at the end of its curly brace block.
+// 3. Keep names or rates within quotes if they contain words, numbers can just be written directly.
+
+const PRODUCT_CATALOG = [
+  {
+    sku: "SAI001",
+    name: "Aerocomfort Sheets",
+    description: "Brick red XLPE 13mm Foam",
+    size: "3.5ft x 18ft",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 6210
+  },
+  {
+    sku: "SAI002",
+    name: "Aerocomfort Sheets",
+    description: "Brick red XLPE 13mm Foam",
+    size: "3.5ft x 16ft",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 5520
+  },
+  {
+    sku: "SAI020",
+    name: "UPVC Bamboo Profile Sheets",
+    description: "Prospan Brick Red",
+    size: "3.5ft x 18ft",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 2700
+  },
+  {
+    sku: "SAI024",
+    name: "UPVC Bamboo Profile Sheets",
+    description: "Prospan Brick Red",
+    size: "3.5ft x 10ft",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 1500
+  },
+  {
+    sku: "SAI027",
+    name: "UPVC Bamboo Accessories",
+    description: "Main Ridges Brick Red",
+    size: "N/A x 1 Mtr",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 750
+  },
+  {
+    sku: "SAI038",
+    name: "UPVC Bamboo Profile Sheets",
+    description: "Prospan Grey",
+    size: "3.5ft x 10ft",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 1500
+  },
+  {
+    sku: "SAI043",
+    name: "UPVC Bamboo Accessories",
+    description: "Valley Plain Ridges Grey",
+    size: "N/A x 2 Mtr",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 1300
+  },
+  {
+    sku: "SAI044",
+    name: "UPVC Bamboo Accessories",
+    description: "Diagnol Ridges End Cover Grey",
+    size: "N/A x N/A",
+    type: "Roofing",
+    unit: "Nos",
+    rateInclGst: 550
+  }
+];
